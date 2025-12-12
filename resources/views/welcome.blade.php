09b9d9be7a6c86744d2f653014ac56ad4f602a68
Deshan Kodithuwakku <62970260+deshankodithuwakku@users.noreply.github.com>
2025-12-12 20:52:33 +0530

    add the logo in title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>LTBio - Lakshitha Thennakoon</title>
+  <link rel="icon" type="image/jpeg" href="{{ asset('images/logo1.jpeg') }}" />
   <link rel="preconnect" href="https://fonts.bunny.net">
   <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600,700" rel="stylesheet" />
 
